174c97c1b2b29822ad39fd04c992cb3de3824fb4
Sagun <sagunsiwakoti25@gmail.com>
2026-03-20 21:32:09 +0545

    Updates sidebar to support roles navigation

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
 import { Link } from '@inertiajs/react';
-import { BookOpen, FolderGit2, LayoutGrid, Lock } from 'lucide-react';
+import { BookOpen, FolderGit2, LayoutGrid, Lock, LockKeyhole } from 'lucide-react';
 import AppLogo from '@/components/app-logo';
 import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
