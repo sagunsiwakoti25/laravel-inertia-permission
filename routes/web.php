955696f5b9e1a61433144e80b3dba8fe8ef8afb3
Sagun <sagunsiwakoti25@gmail.com>
2026-03-20 21:32:09 +0545

    Prepares routes for roles feature integration

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,16 @@ Route::inertia('/', 'welcome', [
 ])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+
+    //Permissions Routes
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
     Route::get('permissions', [PermissionController::class, 'index'])->name('permissions.index');
     Route::post('permissions', [PermissionController::class, 'store'])->name('permissions.store');
     Route::put('permissions/{permission}', [PermissionController::class, 'update'])->name('permissions.update');
     Route::delete('permissions/{permission}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
-});
+
+    //Roles Routes
+
+    });
 
 require __DIR__.'/settings.php';
